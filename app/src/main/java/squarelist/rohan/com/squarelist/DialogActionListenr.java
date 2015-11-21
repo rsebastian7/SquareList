584b9d22e5070c4458b8b6d278cd726ebe6dbfe7
rsebastian7 <rsebastian7@gmail.com>
2015-11-21 16:53:42 -0500

    Updates 11/21/15

---
+++ app/src/main/java/squarelist/rohan/com/squarelist/DialogActionListenr.java
@@ -0,0 +1,12 @@
+package squarelist.rohan.com.squarelist;
+
+import android.os.Bundle;
+
+/**
+ * Created by Rohan on 11/15/2015.
+ */
+public interface DialogActionListenr {
+    public void onOKSelected(Bundle b);
+    public void onCancelSelected();
+
+}
